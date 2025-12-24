90c8aa4ee1d2254e2ddd852ef52ed5951fbc3649
kundan@54321 <kundanchapagain123@gmail.com>
2025-12-24 13:40:12 +0545

    updated

--- ams/views.py
+++ ams/views.py
@@ -16,9 +16,11 @@ from django.conf import settings
 import os
 from rest_framework import status 
 from .serializer import AdduserSerializer
+from rest_framework.permissions import AllowAny
 #--------------------------
 # user login 
 #--------------------------
+@permission_classes([AllowAny])
 @api_view(['POST'])
 def user_login_api(request):
     username = request.data.get('username', '').strip()
