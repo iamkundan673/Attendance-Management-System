fac7a4b16500e1d8605f8d0cb5b09829b8470f63
kundan@54321 <kundanchapagain123@gmail.com>
2025-12-24 12:25:18 +0545

    updated

--- account/views.py
+++ account/views.py
@@ -5,12 +5,13 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import AllowAny
 
 
 # admin login
 @csrf_exempt
 @api_view(['POST'])
-# @permission_classes([IsAdminUser])
+@permission_classes([AllowAny])
 def user_login_api(request):
     username = request.data.get('username', '').strip()
     password = request.data.get('password', '').strip()
