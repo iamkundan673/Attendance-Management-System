250d73635f28c9d6cc9f940d7b8290b2f52369d1
kundan@54321 <kundanchapagain123@gmail.com>
2026-01-20 14:28:43 +0545

    Structure code file

--- ams/urls/user.py
+++ ams/urls/user.py
@@ -5,7 +5,7 @@ from ams.views.User import upload_profile_picture_api,get_profile_picture_api
 from ams.views.Holiday import holiday_list_api
 urlpatterns = [
     path('dashboard/', dashboard_api, name='user-dashboard'),
-    path('attendance/', attendance_api, name='mark_attendance'),
+    path('mark-attendance/', attendance_api, name='mark_attendance'),
     path("auto-mark-absent/<str:secret_key>/", auto_mark_absent, name="auto_mark_absent"),
     path('upload-profile-picture/<int:user_id>/',upload_profile_picture_api,name='upload_profile_picture_api'),
     path('holiday-list/',holiday_list_api,name='holiday_list_api'),
